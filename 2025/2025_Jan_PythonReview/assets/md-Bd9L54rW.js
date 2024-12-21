2315183ad511d40b03babb88754ef29a1a9247b8
INFOstudio <beta-s@outlook.com>
2024-12-21 21:27:21 +0800

    push

---
+++ 2025/2025_Jan_PythonReview/assets/md-Bd9L54rW.js
@@ -0,0 +1,5 @@
+import{o as n,c as a,k as l,e,q as i,s as p,B as s}from"./modules/vue-B36rZNRv.js";import{I as m}from"./slidev/default-r6dwE-95.js";import{u,f as c}from"./slidev/context-B3qlNpu_.js";import"./index-DwgPqF1J.js";import"./modules/shiki-BEMsoB1H.js";const g={__name:"slides.md__slidev_3",setup(d){const{$slidev:f,$nav:_,$clicksContext:o,$clicks:$,$page:v,$renderContext:x,$frontmatter:r}=u();return o.setup(),(k,t)=>(n(),a(m,i(p(s(c)(s(r),2))),{default:l(()=>t[0]||(t[0]=[e("h1",null,"赋值",-1),e("div",{style:{"font-size":"3em"}},[e("pre",null,[e("code",null,`
+a = 1
+`)])],-1),e("div",{style:{"font-size":"3em"}},[e("pre",null,[e("code",null,`
+变量名 = 值
+`)])],-1)])),_:1},16))}};export{g as default};
