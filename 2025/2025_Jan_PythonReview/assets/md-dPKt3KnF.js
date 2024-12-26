28d3c99ed83131640bb78f696fabe783f7658b92
INFOstudio <beta-s@outlook.com>
2024-12-27 02:30:29 +0800

    udpate

---
+++ 2025/2025_Jan_PythonReview/assets/md-dPKt3KnF.js
@@ -0,0 +1,5 @@
+import{o as n,c as a,k as l,e,q as i,s as p,B as s}from"./modules/vue-wjw_P99p.js";import{I as m}from"./slidev/default-nObsT9n1.js";import{u,f as c}from"./slidev/context-hZA4rvP9.js";import"./index-BPUAOrcD.js";import"./modules/shiki-B9PZdNSg.js";const g={__name:"slides.md__slidev_3",setup(d){const{$slidev:f,$nav:_,$clicksContext:o,$clicks:$,$page:v,$renderContext:x,$frontmatter:r}=u();return o.setup(),(k,t)=>(n(),a(m,i(p(s(c)(s(r),2))),{default:l(()=>t[0]||(t[0]=[e("h1",null,"赋值",-1),e("div",{style:{"font-size":"3em"}},[e("pre",null,[e("code",null,`
+a = 1
+`)])],-1),e("div",{style:{"font-size":"3em"}},[e("pre",null,[e("code",null,`
+变量名 = 值
+`)])],-1)])),_:1},16))}};export{g as default};
